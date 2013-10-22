e88c7c1a4d5f1db38b8666c87a969932b07dd66b
Will <willusher.life@gmail.com>
2013-10-21 21:14:36 -0700

    Just draw the f_color to the diffuse map

--- res/fshader.glsl
+++ res/fshader.glsl
@@ -8,7 +8,7 @@ layout(location = 0) out vec3 diffuse;
 layout(location = 1) out vec3 normal;
 
 void main(){
-	diffuse = 0.6f * f_color + 0.4f * vec3(f_uv, 1.f);
+	diffuse = f_color;
 	//Scale the normal values from [-1,1] range to [0,1] range
 	normal = ((f_normal - vec3(-1.f, -1.f, -1.f) / 2.f));
 }
