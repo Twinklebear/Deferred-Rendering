5cd0a3fb41d324104050508f4b2bb6d25fc4835d
Will <willusher.life@gmail.com>
2013-11-14 21:46:39 -0800

    Adding more detailed debug output information

--- src/util.cpp
+++ src/util.cpp
@@ -2,10 +2,18 @@
 #include <array>
 #include <map>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 #include <GL/glew.h>
 #include <glm/glm.hpp>
+
+#ifdef __linux__
+#include <SDL2/SDL.h>
+#elif defined(_WIN32)
+#include <SDL.h>
+#endif
+
 #include "util.h"
 
 std::string util::readFile(const std::string &fName){
@@ -95,8 +103,61 @@ bool util::logGLError(const std::string &msg){
 void util::glDebugCallback(GLenum src, GLenum type, GLuint id, GLenum severity,
 	GLsizei len, const GLchar *msg, GLvoid *user)
 {
-	//TODO: More logging
-	std::cout << "Recieved error msg: " << msg << "\n";
+	//Print a time stamp for the message
+	float sec = SDL_GetTicks() / 1000.f;
+	int min = static_cast<int>(sec / 60.f);
+	sec -= sec / 60.f;
+	std::cout << "[" << min << ":"
+		<< std::setprecision(3) << sec << "] OpenGL Debug -";
+	switch (severity){
+	case GL_DEBUG_SEVERITY_HIGH_ARB:
+		std::cout << " High severity";
+		break;
+	case GL_DEBUG_SEVERITY_MEDIUM_ARB:
+		std::cout << " Medium severity";
+		break;
+	case GL_DEBUG_SEVERITY_LOW_ARB:
+		std::cout << " Low severity";
+	}
+	switch (src){
+	case GL_DEBUG_SOURCE_API_ARB:
+		std::cout << " API";
+		break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM_ARB:
+		std::cout << " Window system";
+		break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER_ARB:
+		std::cout << " Shader compiler";
+		break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY_ARB:
+		std::cout << " Third party";
+		break;
+	case GL_DEBUG_SOURCE_APPLICATION_ARB:
+		std::cout << " Application";
+		break;
+	default:
+		std::cout << " Other";
+	}
+	switch (type){
+	case GL_DEBUG_TYPE_ERROR_ARB:
+		std::cout << " Error";
+		break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB:
+		std::cout << " Deprecated behavior";
+		break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB:
+		std::cout << " Undefined behavior";
+		break;
+	case GL_DEBUG_TYPE_PORTABILITY_ARB:
+		std::cout << " Portability";
+		break;
+	case GL_DEBUG_TYPE_PERFORMANCE_ARB:
+		std::cout << " Performance";
+		break;
+	default:
+		std::cout << " Other";
+	}
+	std::cout << ":\n\t" << msg << "\n";
 }
 bool util::loadOBJ(const std::string &fName, GLuint &vbo, GLuint &ebo, size_t &nElems){
 	std::ifstream file(fName);
